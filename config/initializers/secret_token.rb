66ae0e034d15a0d001155c1236c495795e90fc78
Guilherme Berger <guilherme.berger@gmail.com>
2014-01-02 17:29:08 -0200

    rails new pqp -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pqp::Application.config.secret_key_base = '5eda752d587c83ca683f617189e73aec8655240b4234c4a3cd3f1db2b243e6b895ca13c1c5ef528e7cf5e82dd617d16fe830b50f2582f975d2006cc38272b0f4'
