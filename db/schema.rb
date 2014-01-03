fe9905a62b3dda84312a56b12d80519be56c74ff
Guilherme Berger <guilherme.berger@gmail.com>
2014-01-03 01:01:37 -0200

    Add is_admin to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140102215736) do
+ActiveRecord::Schema.define(version: 20140103025337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140102215736) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_admin"
   end
 
 end
