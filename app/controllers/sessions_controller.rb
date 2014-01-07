d50347a728e96dab5cd27833bc525eee7d48ba3c
Guilherme Berger <guilherme.berger@gmail.com>
2014-01-07 04:22:22 -0200

    Redirect to where the user was before logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to(request.env['omniauth.origin'] || root_path)
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to(request.env['omniauth.origin'] || root_path)
   end
 end
