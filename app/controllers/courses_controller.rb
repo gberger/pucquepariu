80b3bf624d1ebecf84458a485a3f47da827ed1cb
Guilherme Berger <guilherme.berger@gmail.com>
2014-01-07 00:44:51 -0200

    Remove set_course private method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,4 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
-  
   # GET /courses
   def index
     @courses = Course.all
@@ -8,6 +6,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1
   def show
+    @course = Course.find_by_abbreviation(params[:id])
   end
 
   # GET /courses/new
@@ -17,6 +16,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1/edit
   def edit
+    @course = Course.find_by_abbreviation(params[:id])
   end
 
   # POST /courses
@@ -32,6 +32,8 @@ class CoursesController < ApplicationController
 
   # PATCH/PUT /courses/1
   def update
+    @course = Course.find_by_abbreviation(params[:id])
+
     if @course.update(course_params)
       redirect_to @course, notice: 'course was successfully updated.'
     else
@@ -41,16 +43,12 @@ class CoursesController < ApplicationController
 
   # DELETE /courses/1
   def destroy
+    @course = Course.find_by_abbreviation(params[:id])
     @course.destroy
     redirect_to courses_url
   end
 
 private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_course
-    @course = Course.find_by_abbreviation(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def course_params
     params.require(:course).permit(:abbreviation, :name, :credits)
