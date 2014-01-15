0ce244b06e68b4acdc16d60c6d260d2d54be2220
Guilherme Berger <guilherme.berger@gmail.com>
2014-01-15 02:19:41 -0200

    Tweaks on Markdown rendering and cheatsheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,20 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, no_intra_emphasis: true)
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      footnotes: true,
+      supercript: true
+    }
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(link_attributes: {"target" => "_blank"}), options)
     html = renderer.render text
 
     doc = Nokogiri::HTML(html)
-    doc.css('a').each do |a|
-      a["target"] = "_blank"
-    end
+    doc.css('table').add_class('table table-striped table-bordered table-hover')
     
     doc.css('body').to_s.html_safe
   end
