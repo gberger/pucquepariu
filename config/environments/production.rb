15955dd59d4ea057cc61de13ade201a78870ac29
Guilherme Berger <guilherme.berger@gmail.com>
2014-02-09 22:16:59 -0200

    Add public.css and admin.css to compilation. Oops\!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Pqp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( public.js admin.js )
+  config.assets.precompile += %w( public.js admin.js public.css admin.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
