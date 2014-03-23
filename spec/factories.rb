ab002146822303b4174dcb91540575cd37e0ac7b
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 20:17:37 -0300

    Add validation tests to ChatMessage using shoulda-matchers

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,12 +2,35 @@
 
 FactoryGirl.define do
   factory :user do
+    provider 'facebook'
+    uid { |n| n }
+    name 'Some guy'
+    nickname 'smgy'
+    email 'someguy@facebook.com'
 
+    oauth_token { |n| n }
+    oauth_expires_at 2.months.from_now
+
+    role "basic"
+
+    factory :teacher do
+      role "teacher"
+    end
+
+    factory :admin do
+      role "admin"
+    end
   end
 
   factory :course do
     abbreviation { |n| "INF100#{n}" }
     name { |n| "Programação #{n}" }
     credits 4
+  end
+
+  factory :chat_message do
+    course
+    user
+    text "This is some text."
   end
 end
--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe ChatMessage do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:course) }
+    it { should validate_presence_of(:text) }
+    it { should ensure_length_of(:text).is_at_least(1).is_at_most(512) }
+  end
 end
