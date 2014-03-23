9e67fa27289a6f7d80d970b7a9408152a55a7cc7
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 20:17:06 -0300

    Add presence validation to ChatMessage

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -3,9 +3,10 @@ class ChatMessage < ActiveRecord::Base
   belongs_to :user
 
   validates :text, length: {minimum: 1, maximum: 512}
+  validates_presence_of :user, :course, :text
 
   def as_json(options)
-    {course: course.abbreviation, name: user.name, msg: text, timestamp: created_at.ms, id: id}
+    {course: course.abbreviation, name: user.name, msg: text, timestamp: timestamp, id: id}
   end
 
   def timestamp
