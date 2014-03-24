672fc0cdb81dac6a588e04bcbe5316574ccb43dc
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 23:19:50 -0300

    Clean controllers and views tests

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,53 +2,11 @@ require 'spec_helper'
 
 describe CoursesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
+  describe 'GET #index' do
+    before { get :index }
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
+    it { should respond_with(200) }
+    it { should render_template('index') }
   end
 
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe StaticController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
+  describe 'get #index' do
+    before { get :index }
+
+    it { should respond_with(200) }
+    it { should render_template('index') }
+  end
+
+  describe 'get #ping' do
+    before { get :ping }
+
+    it { should respond_with(200) }
   end
 
 end
