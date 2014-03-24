76e0ca71128f25ce865a59788b0218745c344df6
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 21:31:32 -0300

    Remove ChatMessage#as_json overload

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -5,10 +5,6 @@ class ChatMessage < ActiveRecord::Base
   validates :text, length: {minimum: 1, maximum: 512}
   validates_presence_of :user, :course, :text
 
-  def as_json(options)
-    {course: course.abbreviation, name: user.name, msg: text, timestamp: timestamp, id: id}
-  end
-
   def timestamp
     created_at.ms
   end
