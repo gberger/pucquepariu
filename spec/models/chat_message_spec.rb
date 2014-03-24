660823bc1e1971ba1aeaace78f7ad920798cd405
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 21:31:21 -0300

    Add tests for ChatMessage model relations

--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe ChatMessage do
+  describe 'relations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:course) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:user) }
     it { should validate_presence_of(:course) }
