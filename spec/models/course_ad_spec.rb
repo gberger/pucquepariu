8b92e2a4dd48d27bc5d17cab4a8de0417b338e1d
Guilherme Berger <guilherme.berger@gmail.com>
2014-03-23 21:48:34 -0300

    Add testes to CourseAd model

--- spec/models/course_ad_spec.rb
+++ spec/models/course_ad_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe CourseAd do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relations' do
+    it { should belong_to(:course) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:course) }
+    it { should validate_presence_of(:image) }
+    it { should validate_presence_of(:link) }
+  end
 end
