3e39c53e6d9286eceda4c96e864f1679ffdbdfc2
Guilherme Berger <guilherme.berger@gmail.com>
2014-04-01 11:46:30 -0300

    add monkey patch: Enumerable#group_by_recursive

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -27,3 +27,16 @@ class Time
     Date.parse(self.to_s)
   end
 end
+
+module Enumerable
+  def group_by_recursive(*props)
+    groups = group_by(&props.first)
+    if props.count == 1
+      groups
+    else
+      groups.merge(groups) do |group, elements|
+        elements.group_by_recursive(*props.drop(1))
+      end
+    end
+  end
+end
