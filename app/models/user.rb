7b8aff09178cdbfbae145b8a8cf4a8b49a828fa5
Guilherme Berger <guilherme.berger@gmail.com>
2016-06-27 22:29:11 -0300

    remove nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.nickname = auth.info.nickname
       user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
